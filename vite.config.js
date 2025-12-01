b18b5a327bb94744c5147d0d440f79f6121bdb4c
yujiaping <2507490921@qq.com>
2025-12-01 21:27:54 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000
+    }
   }
 })
